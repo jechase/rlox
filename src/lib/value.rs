a933dfc86a7d569f6fe8dc7d99bda4620453ff18
Josh Chase <josh@jec.pw>
2018-09-12 19:33:10 -0400

    Value Display

--- src/lib/value.rs
+++ src/lib/value.rs
@@ -1,9 +1,15 @@
 use crate::*;
 
-#[derive(Debug, Clone)]
+use display_derive::Display;
+
+#[derive(Debug, Clone, PartialEq, Display)]
 pub enum Value {
+    #[display(fmt = "nil")]
     Nil,
+    #[display(fmt = "{:?}", _0)]
     String(LoxStr),
+    #[display(fmt = "{:?}", _0)]
     Number(f64),
+    #[display(fmt = "{:?}", _0)]
     Bool(bool),
 }
