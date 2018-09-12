9e870665864f6b3ae84f36897466cfca7254e936
Josh Chase <josh@jec.pw>
2018-09-12 19:32:06 -0400

    Error updates

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,7 +1,4 @@
-use failure::{
-    Error,
-    Fail,
-};
+use failure::Fail;
 
 use display_derive::Display;
 
@@ -12,15 +9,27 @@ use std::fmt::{
 
 use crate::*;
 
-#[derive(Default, Debug)]
-pub struct Reporter {
-    errors: Vec<Error>,
+#[derive(Debug)]
+pub struct Reporter<E> {
+    errors: Vec<E>,
+}
+
+impl<E> Default for Reporter<E>
+where
+    E: Fail,
+{
+    fn default() -> Self {
+        Reporter::new()
+    }
 }
 
 #[derive(Fail, Debug)]
-pub struct Errors(Vec<Error>);
+pub struct Errors<E: Fail>(Vec<E>);
 
-impl Display for Errors {
+impl<E> Display for Errors<E>
+where
+    E: Fail,
+{
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         for (idx, err) in self.0.iter().enumerate() {
             if idx == self.0.len() - 1 {
@@ -33,8 +42,17 @@ impl Display for Errors {
     }
 }
 
-impl Reporter {
-    pub fn finish(self) -> Result<(), Errors> {
+impl<E> Reporter<E>
+where
+    E: Fail,
+{
+    pub fn new() -> Reporter<E> {
+        Reporter {
+            errors: vec![],
+        }
+    }
+
+    pub fn finish(self) -> Result<(), Errors<E>> {
         if self.errors.len() == 0 {
             Ok(())
         } else {
@@ -42,20 +60,46 @@ impl Reporter {
         }
     }
 
-    pub fn report<E>(&mut self, error: E)
+    pub fn report(&mut self, error: E) {
+        self.errors.push(error);
+    }
+
+    pub fn filter<'s, 'i, 'r, I, T>(
+        &'s mut self,
+        i: I,
+    ) -> impl Iterator<Item = T> + 'r
     where
-        E: Into<Error>,
+        I: IntoIterator<Item = Result<T, E>> + 'i,
+        'i: 'r,
+        's: 'r,
     {
-        self.errors.push(error.into());
+        i.into_iter().filter_map(move |res| match res {
+            Ok(t) => Some(t),
+            Err(e) => {
+                self.report(e);
+                None
+            },
+        })
+    }
+
+    pub fn join<T>(&mut self, mut other: Reporter<T>)
+    where
+        E: From<T>,
+    {
+        self.errors.extend(other.errors.drain(..).map(From::from));
     }
 }
 
 #[derive(Fail, Debug, Display)]
-#[display(fmt = "[line {}] Error{}: {}", line, loc, msg)]
-pub struct LoxError {
-    line: usize,
-    loc:  String,
-    msg:  String,
+pub enum LoxError {
+    #[display(fmt = "[line {}] Error: {}", _0, _1)]
+    Scan(usize, String),
+    #[display(fmt = "[line {}] Error{}: {}", _0, _1, _2)]
+    Parse(usize, String, String),
+    #[display(fmt = "{}", _0)]
+    Cast(String),
+    #[display(fmt = "[line {}] Error: {}", _0, _1)]
+    Runtime(usize, String),
 }
 
 impl LoxError {
@@ -63,11 +107,7 @@ impl LoxError {
     where
         S: Into<String>,
     {
-        LoxError {
-            line,
-            loc: "".into(),
-            msg: msg.into(),
-        }
+        LoxError::Scan(line, msg.into())
     }
 
     pub fn parse<S>(token: &Token, msg: S) -> LoxError
@@ -79,10 +119,20 @@ impl LoxError {
         } else {
             format!(" at {:?}", token.lexeme)
         };
-        LoxError {
-            line: token.line,
-            loc,
-            msg: msg.into(),
-        }
+        LoxError::Parse(token.line, loc, msg.into())
+    }
+
+    pub fn runtime<S>(token: &Token, msg: S) -> LoxError
+    where
+        S: Into<String>,
+    {
+        LoxError::Runtime(token.line, msg.into())
+    }
+
+    pub fn typecast<S>(msg: S) -> LoxError
+    where
+        S: Into<String>,
+    {
+        LoxError::Cast(msg.into())
     }
 }
